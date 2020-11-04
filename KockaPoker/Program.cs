0a5e9e84ef9d7d10ab71ef1514b65ed7a1ebd8fa
ErdosXenia <erdos.xenia@gmail.com>
2020-11-04 14:52:16 +0100

    Add project files.

---
+++ KockaPoker/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KockaPoker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dobas d = new Dobas(3,3,4,3,3);
+            
+            //d.EgyDobas();
+            d.Kiiras();
+            Console.WriteLine(d.Erteke()); 
+
+            Console.ReadKey();
+        }
+    }
+}
